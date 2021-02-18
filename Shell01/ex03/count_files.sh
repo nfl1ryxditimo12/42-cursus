3e47a3f5a9faa5d301d2a63aa6e9e451ddfd6602
Seongsu Kim <seonkim@c8r4s8.42seoul.kr>
2021-02-18 23:34:13 +0900

    Shell_01

--- Shell01/ex03/count_files.sh
+++ Shell01/ex03/count_files.sh
@@ -1,2 +1,1 @@
-#!/bin/bash
 find ./ | wc -l | tr -d ' '
--- Shell01/ex07/r_dwssap.sh
+++ Shell01/ex07/r_dwssap.sh
@@ -1,1 +1,1 @@
-cat /etc/passwd | grep -v "^#" | sed -n "2, \$p" | cut -f 1 -d ':' | rev | sort -r | sed -n "$FT_LINE1, $FT_LINE2 p" | tr '\n' ',' | sed 's/,/, /g' | sed 's/, $/./g'
+cat /etc/passwd | sed '/#/d' | awk '{if (NR%2==0) print}' | sed 's/:.*//' | rev | sort -r | sed -n "$FT_LINE1, $FT_LINE2 p" | tr '\n' ',' | sed 's/,/, /g' | sed 's/, $/./g' | tr -d '\n'
