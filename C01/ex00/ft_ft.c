71af15c43937ce3a88b4ef7b4f1d30f0cc717e9c
Seongsu Kim <seonkim@c8r2s6.42seoul.kr>
2021-02-21 14:18:45 +0900

    C00

--- C01/ex00/ft_ft.c
+++ C01/ex00/ft_ft.c
@@ -1,39 +1,16 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_putnbr.c                                        :+:      :+:    :+:   */
+/*   ft_ft.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: seonkim <seonkim@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/02/21 01:42:00 by seonkim           #+#    #+#             */
-/*   Updated: 2021/02/21 13:15:15 by seonkim          ###   ########.fr       */
+/*   Created: 2021/02/21 11:24:55 by seonkim           #+#    #+#             */
+/*   Updated: 2021/02/21 11:44:32 by seonkim          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <unistd.h>
-
-void	itoa(int nb)
+void	ft_ft(int *nbr)
 {
-	char	ch_nb;
-
-	if (nb == 0)
-		return ;
-	itoa(nb / 10);
-	if (nb > 0)
-	{
-		ch_nb = nb % 10 + '0';
-		write(1, &ch_nb, 1);
-	}
-	else if (nb < 0)
-	{
-		ch_nb = -(nb % 10) + '0';
-		write(1, &ch_nb, 1);
-	}
-}
-
-void	ft_putnbr(int nb)
-{
-	if (nb < 0)
-		write(1, "-", 1);
-	itoa(nb);
+	*nbr = 42;
 }
