6a3b1f10d66a9d700daefeb86f25fa35a65ccb8f
seongsu <nfl1ryxditimo12@gmail.com>
2021-02-22 17:34:08 +0900

    C02

--- C02/ex12/ft_print_memory.c
+++ C02/ex12/ft_print_memory.c
@@ -3,11 +3,26 @@
 /*                                                        :::      ::::::::   */
 /*   ft_print_memory.c                                  :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: seonkim <seonkim@student.42seoul.kr>       +#+  +:+       +#+        */
+/*   By: seongsu <seongsu@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/22 17:14:16 by seonkim           #+#    #+#             */
-/*   Updated: 2021/02/22 17:14:18 by seonkim          ###   ########.fr       */
+/*   Updated: 2021/02/22 17:22:56 by seongsu          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <unistd.h>
 
+int main()
+{
+    char b;
+    char *ptr;
+    int i;
+
+    ptr = &b;
+    i = 0;
+    while (ptr[i] != '\0')
+    {
+        write(1, &ptr[i], 1);
+        i++;
+    }
+}
