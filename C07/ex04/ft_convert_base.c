138744839367e63ea5141a60401129b30b6171ec
Seongsu Kim <seonkim@c10r4s12.42seoul.kr>
2021-03-07 23:04:14 +0900

    Piscine

--- C07/ex04/ft_convert_base.c
+++ C07/ex04/ft_convert_base.c
@@ -6,8 +6,13 @@
 /*   By: seonkim <seonkim@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/04 17:09:09 by seonkim           #+#    #+#             */
-/*   Updated: 2021/03/04 17:15:37 by seonkim          ###   ########.fr       */
+/*   Updated: 2021/03/07 21:47:55 by seonkim          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+char *ft_convert_base(char *nbr, char *base_from, char *base_to)
+{
+	char	*ret;
+	int		check_minus;
 
+}
