4d209938ef6d64318d260c8d0dcae4189075721f
Seongsu Kim <seonkim@c7r5s7.42seoul.kr>
2021-03-08 14:43:49 +0900

    Piscine

--- C00/ex08/ft_print_combn.c
+++ C00/ex08/ft_print_combn.c
@@ -6,36 +6,30 @@
 /*   By: seonkim <seonkim@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/21 17:54:44 by seonkim           #+#    #+#             */
-/*   Updated: 2021/02/21 18:35:39 by seonkim          ###   ########.fr       */
+/*   Updated: 2021/03/08 05:03:00 by seonkim          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 
-void combine_n(int n, char *c)
+void 
+
+void combine_n(int i, int n)
 {
-	int i;
-	int level;
-	char ch_i;
-	
-	level = 1;
-	while (i <= 9)
+	char arr[10];
+
+	arr[i] = '0';
+}
+
+void ft_print_combn(int n);
+{
+	char	arr[10];
+	int		i;
+
+	arr[0] = '0';
+	i = -1;
+	while (++i < n)
 	{
-	 	ch_i = i + '0';
-		combine_n(int n, int *level);
+		combine_n(i, n);
 	}
-}
-
-void print_n(char *c, int *i, int a, int b)
-{
-	write(
-}
-
-int	main(void)
-{
-	int	n;
-	char c[10];
-	
-	n = 2;
-	c[0] = '0';
 }
--- C05/ex02/ft_iterative_power.c
+++ C05/ex02/ft_iterative_power.c
@@ -6,7 +6,7 @@
 /*   By: seonkim <seonkim@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/02 17:17:41 by seonkim           #+#    #+#             */
-/*   Updated: 2021/03/08 01:55:17 by seonkim          ###   ########.fr       */
+/*   Updated: 2021/03/08 04:19:34 by seonkim          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,15 +14,12 @@ int	ft_iterative_power(int nb, int power)
 {
 	int num;
 
-	num = 1;
-	if (nb < 0)
+	num = nb;
+	if (power < 0)
 		return (0);
 	if (power == 0)
 		return (1);
-	while (power > 0)
-	{
+	while (--power)
 		num *= nb;
-		power--;
-	}
 	return (num);
 }
