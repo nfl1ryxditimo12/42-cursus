297a6b9d1c005afdc4448e7a21fb38c20613bc64
Seongsu Kim <seonkim@c10r1s12.42seoul.kr>
2021-03-14 00:26:53 +0900

    Piscine

--- C09/ex00/ft_swap.c
+++ C09/ex00/ft_swap.c
@@ -6,7 +6,7 @@
 /*   By: seonkim <seonkim@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/03 20:20:24 by seonkim           #+#    #+#             */
-/*   Updated: 2021/03/03 20:21:16 by seonkim          ###   ########.fr       */
+/*   Updated: 2021/03/14 00:26:43 by seonkim          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,5 +15,5 @@ void	ft_swap(int *a, int *b)
 	int temp;
 	temp = *a;
 	*a = *b;
-	*a = temp;
+	*b = temp;
 }
