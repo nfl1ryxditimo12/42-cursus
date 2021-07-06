37c84eb33982245fe5c3314b0c9d9d3e7ed4b849
nfl1ryxditimo12 <nfl1ryxditimo12@gmail.com>
2021-07-06 12:27:31 +0900

    redirection

--- parse.c
+++ parse.c
@@ -6,7 +6,7 @@
 /*   By: seonkim <seonkim@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/01 15:41:25 by seonkim           #+#    #+#             */
-/*   Updated: 2021/07/02 13:44:12 by seonkim          ###   ########seoul.kr  */
+/*   Updated: 2021/07/06 12:27:21 by seonkim          ###   ########seoul.kr  */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,20 +14,50 @@
 
 int     chk_symbol(char *line)
 {
-    if (*line == '<')
-        if (*(line + 1) == '<')
-            return (2);
-    if (*line == '>')
-        if (*(line + 1) == '>')
-            return (2);
     if (*line == '|')
         if (*(line + 1) == '|')
             return (2);
     if (*line == '&')
         if (*(line + 1) == '&')
             return (2);
-    if (*line == '<' || *line == '>' || *line == '|')
+    if (*line == '|')
         return (1);
+    return (0);
+}
+
+int     is_num(char c)
+{
+    if ('0' <= c && c <= '9')
+        return (1);
+    return (0);
+}
+
+int     chk_redirect(char *line)
+{
+    int cnt;
+
+    cnt = 0;
+    if (*line == '<' && *(line + 1) == '<')
+        return (2);
+    if (*line == '>' && *(line + 1) == '>')
+        return (2);
+    if (*line == '<' || *line == '>')
+        return (1);
+    return (0);
+}
+
+int     check_num(char *line)
+{
+    int cnt;
+
+    cnt = 0;
+    while (is_num(*line))
+    {
+        line++;
+        cnt++;
+    }
+    if (*line == '<' || *line == '>')
+        return (cnt + 1);
     return (0);
 }
 
@@ -77,11 +107,18 @@ void    process_split(t_token *ptr, char *line)
             line += chk_symbol(line);
             flag = 2;
         }
+        if (check_num(line) || chk_redirect(line))
+        {
+            if (flag == 1 || flag == 2 || flag == 3)
+                ptr = ptr->next;
+            line += line_cpy(ptr, line);
+            flag = 3;
+        }
         if (*line && !(*line == 32 || *line == 9) && !chk_symbol(line))
         {
-            if (flag == 2)
+            if (flag == 2 || flag == 3)
                 ptr = ptr->next;
-            line_cpy(ptr, line);
+            line += line_cpy(ptr, line);
             while (*line && !(*line == 32 || *line == 9) && !chk_symbol(line))
                 line++;
             flag = 1;
