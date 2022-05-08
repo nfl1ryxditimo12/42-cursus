2301e3846fa398e88468a0fc727d4d10bc384bfc
Seongsu Kim <seonkim@c1r1s1.42seoul.kr>
2022-05-08 21:02:44 +0900

    implement vector

--- includes/vector.hpp
+++ includes/vector.hpp
@@ -14,6 +14,7 @@ namespace ft
 		/*************************************/
 		/*           Define Types            */
 		/*************************************/
+
 		public:
 			/* Type define */
 			typedef _Tp                     value_type;
@@ -34,6 +35,7 @@ namespace ft
 		/*************************************/
 		/*         Member Variables          */
 		/*************************************/
+
 		private:
 			allocator_type  _alloc;
 			pointer         _begin;
@@ -43,10 +45,13 @@ namespace ft
 		/*************************************/
 		/*          Public Function          */
 		/*************************************/
+
 		public:
+
 			/*************************************/
 			/*            Constructor            */
 			/*************************************/
+
 			vector(const allocator_type &alloc = allocator_type())
 			: _alloc(alloc), _begin(NULL), _end(NULL), _end_cap(NULL) {}
 
@@ -81,11 +86,13 @@ namespace ft
 			/*************************************/
 			/*             Destructor            */
 			/*************************************/
+
 			~vector() { _alloc.deallocate(this->_begin, this->capacity()); };
 
 			/*************************************/
 			/*              Operator             */
 			/*************************************/
+
 			vector &operator=(const vector<value_type> &cls)
 			{
 				if (this != &cls) {
@@ -99,33 +106,73 @@ namespace ft
 			/*************************************/
 			/*             Iterators             */
 			/*************************************/
+
 			iterator begin() const { return this->_begin; }; 
+
 			iterator end() const { return this->_end; };
+
 			iterator rbegin();
+
 			iterator rend();
 
 			/*************************************/
 			/*              Capacity             */
 			/*************************************/
+			
 			size_type   size() const { return this->_end - this->_begin; }
+
 			size_type   max_size() const { return std::min<size_type>(_alloc.max_size(), std::numeric_limits<difference_type>::max()); }
+
 			size_type   capacity() const { return this->_end_cap - this->_begin; }
-			bool        empty() { return this->_begin == NULL; };
-			void        resize(size_type n, value_type val = value_type())
+
+			bool	empty() { return this->_begin == NULL; };
+			
+			void	resize(size_type n, value_type val = value_type())
 			{
 				size_type cs = this->size();
 				if (cs < n)
 					this->append(n - cs, val);
-				else if (cs > n) {}
-					// this->vdestruct(this->begin() + n);
+				else if (cs > n)
+					this->erase(this->begin() + n, this->end());
 			}
 
-			void        reserve();
+			void	reserve(size_type n)
+			{
+				if (n > this->max_size())
+					throw std::out_of_range("Out of range!!");
+				if (n > this->capacity())
+					this->realloc(n);
+			}
 
 			/*************************************/
 			/*           Element access          */
 			/*************************************/
 
+			reference operator[](size_type n) { return *(this->_begin + n); }
+
+			const_reference operator[](size_type n) const { return *(this->_begin + n); }
+
+			reference at (size_type n)
+			{
+				if (n >= this->size())
+					throw std::out_of_range("Out of range");
+				return *(this->begin() + n);
+			}
+
+			const_reference at (size_type n) const
+			{
+				if (n >= this->size())
+					throw std::out_of_range("Out of range");
+				return *(this->begin() + n);
+			}
+
+			reference front() { return *(this->begin()); }
+
+			const_reference front() const { return *(this->begin()); }
+
+			reference back() { return *(this->end()); }
+
+			const_reference back() const { return *(this->end()); }
 
 			/*************************************/
 			/*             Modifiers             */
@@ -133,8 +180,24 @@ namespace ft
 
 			/* assign */
 			template <class InputIterator>
-			void assign(InputIterator first, InputIterator last);
-			void assign(size_type n, const_reference val);
+			void assign(InputIterator first, InputIterator last,
+					typename ft::enable_if<!ft::is_integral<InputIterator>::value, InputIterator>::type = NULL)
+			{
+				size_type new_size = static_cast<size_type>(last - first);
+				if (new_size > this->size())
+					this->realloc(new_size);
+				this->erase(this->_begin, this->_end);
+				for (; first < last; first++, this->_end++)
+					this->_alloc.construct(this->_end, *first);
+			};
+
+			void assign(size_type n, const_reference val)
+			{
+				if (n > this->capacity())
+					this->realloc(n);
+				this->erase(this->_begin, this->_end);
+				this->vconstruct(this->_end + n, val);
+			}
 
 			/* push_back */
 			void push_back(const_reference val)
@@ -145,17 +208,55 @@ namespace ft
 			}
 
 			/* pop_back */
-			void pop_back();
+			void pop_back() { this->erase(this->_end - 1); }
 
 			/* insert */
-			iterator insert(iterator position, const_reference val);
-			void insert(iterator position, size_type n, const_reference val);
+			iterator insert(iterator position, const_reference val)
+			{
+				size_type pos = static_cast<size_type>(position - this->begin());
+				this->move(1, position, this->end());
+				position = this->begin() + pos;
+				this->_alloc.construct(position, val);
+				return position;
+			}
+
+			void insert(iterator position, size_type n, const_reference val)
+			{
+				size_type pos = static_cast<size_type>(position - this->begin());
+				this->move(n, position, this->end());
+				for (size_type i = 0; i < n; i++)
+					this->_alloc.construct(this->begin() + pos + i, val);
+			}
+
 			template <class InputIterator>
-			void insert(iterator position, InputIterator first, InputIterator last);
+			void insert(iterator position, InputIterator first, InputIterator last,
+					typename ft::enable_if<!ft::is_integral<InputIterator>::value, InputIterator>::type = NULL);
 
 			/* erase */
-			iterator erase(iterator position);
-			iterator erase(iterator first, iterator last);
+			iterator erase(iterator position)
+			{
+				iterator last = position + 1;
+				this->_alloc.destroy(position);
+				for (int i = 0; i < _end - last; i++) {
+					_alloc.construct(position + i, *(last + i));
+					_alloc.destroy(last + i);
+				}
+				this->_end -= 1;
+				return position;
+			}
+
+			iterator erase(iterator first, iterator last)
+			{
+				iterator p = first;
+				for (; p < last; p++)
+					this->_alloc.destroy(p);
+				for (int i = 0; i < _end - last; i++) {
+					_alloc.construct(first + i, *(last + i));
+					_alloc.destroy(last + i);
+				}
+				this->_end -= (last - first);
+				return first;
+			}
 
 			/* swap */
 			void swap(vector &cls)
@@ -174,6 +275,7 @@ namespace ft
 		/*************************************/
 		/*         Private Function          */
 		/*************************************/
+
 		private:
 
 			void vconstruct(pointer p, const_reference val)
@@ -198,6 +300,22 @@ namespace ft
 				this->_end_cap = this->_begin + n;
 			}
 
+			void vdestruct(iterator first, iterator last)
+			{
+				for (; first < last; first++) {
+					this->_alloc.destroy(first);
+					this->_alloc.deallocate(first, 1);
+				}
+			}
+
+			void vdestruct(iterator p)
+			{
+				for (; _end >= p; --_end) {
+					this->_alloc.destroy(this->_end);
+					this->_alloc.deallocate(this->_end, 1);
+				}
+			}
+
 			void vdeallocate() { _alloc.deallocate(this->_begin, this->capacity()); }
 
 			void realloc(size_type cap)
@@ -208,15 +326,45 @@ namespace ft
 				this->vallocate(cap);
 				if (tmp._begin != NULL) 
 					this->vconstruct(this->_end + tmp.size(), tmp._begin);
-				std::cout << "test: " << cap << ", " << (this->_end_cap - this->_begin) << std::endl;
 			}
 
 			void append(size_type n, const_reference val)
 			{
 				size_type additional = static_cast<size_type>(this->_end_cap - this->_end);
 				if (additional < n)
-					this->realloc(additional + this->size());
+					this->realloc(n + this->size());
 				this->vconstruct(this->end() + n, val);
+			}
+
+			template <class InputIterator>
+			void move(size_type n, InputIterator first, InputIterator last)
+			{
+				size_type pos = static_cast<size_type>(first - this->begin());
+				size_type move_size = static_cast<size_type>(last - first);
+				pointer tmp = _alloc.allocate(move_size);
+
+				if (this->size() + n > this->capacity())
+					this->realloc(new_capacity(n));
+				for (size_type i = 0; i < move_size; i++)
+					tmp[i] = first[i];
+				for (size_type i = 0; i < move_size; i++) {
+					*(this->begin() + pos + n + i) = tmp[i];
+					this->_alloc.destroy(this->begin() + pos + i);
+				}
+				this->_end += n;
+				std::cout << "new:";
+				for (iterator it = begin(); it < end(); it++)
+					std::cout << " " << *it;
+				std::cout << std::endl;
+				this->_alloc.deallocate(tmp, move_size);
+			}
+
+			size_type new_capacity(size_type n)
+			{
+				size_type new_cap = this->capacity() == 0 ? 1 : this->capacity();
+				while (new_cap < (this->size() + n))
+					new_cap *= 2;
+				return new_cap;
 			}
 	};
 }
--- sources/main.cpp
+++ sources/main.cpp
@@ -30,68 +30,252 @@ void test()
     for (ft::vector<int>::iterator it = v2.begin(); it < v2.end(); it++)
         std::cout << " " << *it;
     std::cout << "\n" << std::endl;
+
+    std::vector<int> v3;
+    std::vector<int> v4;
+
+    v3.push_back(1);
+    v3.push_back(2);
+    v3.push_back(3);
+    v3.push_back(4);
+    v3.push_back(5);
+
+    v4 = v3;
+
+    std::cout << v4.size() << ", " << v4.capacity() << std::endl;
+    std::cout << "v4:";
+    for (std::vector<int>::iterator it = v4.begin(); it < v4.end(); it++)
+        std::cout << " " << *it;
+    std::cout << "\n" << std::endl;
+
+    std::vector<std::string> v5(10);
+    std::vector<std::string> v6;
+
+    std::cout << v5[0] << v5.empty() << std::endl;
+    std::cout << v6.empty() << std::endl;
+
+    std::cout << std::endl;
+
+    std::vector<int> v7(5, 100);
+    ft::vector<int> v8(5, 100);
+
+    std::cout << v7.size() << ", " << v7.capacity() << std::endl;
+    std::cout << "v7:";
+    for (std::vector<int>::iterator it = v7.begin(); it < v7.end(); it++)
+        std::cout << " " << *it;
+    std::cout << "\n" << std::endl;
+
+    std::cout << v8.size() << ", " << v8.capacity() << std::endl;
+    std::cout << "v8:";
+    for (ft::vector<int>::iterator it = v8.begin(); it < v8.end(); it++)
+        std::cout << " " << *it;
+    std::cout << "\n" << std::endl;
+
+    v7.resize(10);
+    v8.resize(10);
+
+    std::cout << v7.size() << ", " << v7.capacity() << std::endl;
+    std::cout << "v7:";
+    for (std::vector<int>::iterator it = v7.begin(); it < v7.end(); it++)
+        std::cout << " " << *it;
+    std::cout << "\n" << std::endl;
+
+    std::cout << v8.size() << ", " << v8.capacity() << std::endl;
+    std::cout << "v8:";
+    for (ft::vector<int>::iterator it = v8.begin(); it < v8.end(); it++)
+        std::cout << " " << *it;
+    std::cout << "\n" << std::endl;
+
+    std::cout << "=================================" << std::endl;
+    std::cout << v7[-1] << std::endl;
+    std::cout << v7[0] << std::endl;
+    std::cout << v7[1] << std::endl;
+    std::cout << v7[2] << std::endl;
+    std::cout << v7[3] << std::endl;
+    std::cout << v7[10] << std::endl;
+    std::cout << "=================================" << std::endl;
+    std::cout << v8[-1] << std::endl;
+    std::cout << v8[0] << std::endl;
+    std::cout << v8[1] << std::endl;
+    std::cout << v8[2] << std::endl;
+    std::cout << v8[3] << std::endl;
+    std::cout << v8[10] << std::endl;
+    std::cout << "=================================\n" << std::endl;
+
+    std::vector<int> t1;
+    ft::vector<int> t3;
+
+    for (int i = 1; i <= 10; i++)
+        t1.push_back(i);
+    std::cout << "size: " << t1.size() << ", " << t1.capacity() << std::endl;
+    std::cout << "t1:";
+    for (std::vector<int>::iterator it = t1.begin(); it < t1.end(); it++)
+        std::cout << " " << *it;
+    std::cout << "\n" << std::endl;
+
+    t1.erase(t1.begin() + 3, t1.begin() + 7);
+    std::cout << "size: " << t1.size() << ", " << t1.capacity() << std::endl;
+    std::cout << "t1:";
+    for (std::vector<int>::iterator it = t1.begin(); it < t1.end(); it++)
+        std::cout << " " << *it;
+    std::cout << "\n" << std::endl;
+
+    for (int i = 1; i <= 10; i++)
+        t3.push_back(i);
+    std::cout << "size: " << t3.size() << ", " << t3.capacity() << std::endl;
+    std::cout << "t3:";
+    for (ft::vector<int>::iterator it = t3.begin(); it < t3.end(); it++)
+        std::cout << " " << *it;
+    std::cout << "\n" << std::endl;
+
+    t3.erase(t3.begin() + 3, t3.begin() + 7);
+
+    std::cout << "size: " << t3.size() << ", " << t3.capacity() << std::endl;
+    std::cout << "t3:";
+    for (ft::vector<int>::iterator it = t3.begin(); it < t3.end(); it++)
+        std::cout << " " << *it;
+    std::cout << "\n" << std::endl;
+    std::cout << *(t3.end()) << ", " << *(t1.end()) << std::endl;
+
+    t1.erase(t1.begin() + 1, t1.end());
+    t3.erase(t3.begin() + 1, t3.end());
+
+
+    std::cout << "size: " << t1.size() << ", " << t1.capacity() << std::endl;
+    std::cout << "t1:";
+    for (std::vector<int>::iterator it = t1.begin(); it < t1.end(); it++)
+        std::cout << " " << *it;
+    std::cout << "\n" << std::endl;
+
+    std::cout << "size: " << t3.size() << ", " << t3.capacity() << std::endl;
+    std::cout << "t3:";
+    for (ft::vector<int>::iterator it = t3.begin(); it < t3.end(); it++)
+        std::cout << " " << *it;
+    std::cout << "\n" << std::endl;
+
+    std::cout << *(t3.end()) << ", " << *(t1.end()) << std::endl;
+
+    std::cout << "\n===================================================\n" << std::endl;
+
+    for (int i = 1; i <= 10; i++)
+        t1.push_back(i);
+    for (int i = 1; i <= 10; i++)
+        t3.push_back(i);
+
+    t1.erase(t1.end() - 1);
+    t3.erase(t3.end() - 1);
+
+
+    std::cout << "size: " << t1.size() << ", " << t1.capacity() << std::endl;
+    std::cout << "t1:";
+    for (std::vector<int>::iterator it = t1.begin(); it < t1.end(); it++)
+        std::cout << " " << *it;
+    std::cout << "\n" << std::endl;
+
+    std::cout << "size: " << t3.size() << ", " << t3.capacity() << std::endl;
+    std::cout << "t3:";
+    for (ft::vector<int>::iterator it = t3.begin(); it < t3.end(); it++)
+        std::cout << " " << *it;
+    std::cout << "\n" << std::endl;
+
+    std::cout << *(t3.end()) << ", " << *(t1.end()) << std::endl;
+
+    std::cout << "\n===================================================\n" << std::endl;
+
+    std::vector<int> res1;
+    ft::vector<int> res2;
+
+    for (int i = 1; i <= 10; i++)
+        res1.push_back(i);
+    for (int i = 1; i <= 10; i++)
+        res2.push_back(i);
+
+    res1.resize(5);
+    res2.resize(5);
+
+    std::cout << "size: " << res1.size() << ", " << res1.capacity() << std::endl;
+    std::cout << "res1:";
+    for (std::vector<int>::iterator it = res1.begin(); it < res1.end(); it++)
+        std::cout << " " << *it;
+    std::cout << "\n" << std::endl;
+
+    std::cout << "size: " << res2.size() << ", " << res2.capacity() << std::endl;
+    std::cout << "res2:";
+    for (ft::vector<int>::iterator it = res2.begin(); it < res2.end(); it++)
+        std::cout << " " << *it;
+    std::cout << "\n" << std::endl;
+
+    std::cout << *(res1.end() + 4) << ", " << *(res2.end() + 4) << std::endl;
+
+    std::cout << "\n===================================================\n" << std::endl;
+
+    std::vector<int> g1(10);
+    ft::vector<int> g2(10);
+
+    g1.reserve(16);
+    g2.reserve(16);
+
+    std::cout << "size: " << g1.size() << ", " << g1.capacity() << std::endl;
+    std::cout << "g1:";
+    for (std::vector<int>::iterator it = g1.begin(); it < g1.end(); it++)
+        std::cout << " " << *it;
+    std::cout << "\n" << std::endl;
+
+    std::cout << "size: " << g2.size() << ", " << g2.capacity() << std::endl;
+    std::cout << "g2:";
+    for (ft::vector<int>::iterator it = g2.begin(); it < g2.end(); it++)
+        std::cout << " " << *it;
+    std::cout << "\n" << std::endl;
+
+    std::cout << "\n===================================================\n" << std::endl;
+
+    std::vector<int> a1;
+
+    for (int i = 1; i <= 10; i++)
+        a1.push_back(i);
+
+    std::cout << "size: " << a1.size() << ", " << a1.capacity() << std::endl;
+    std::cout << "a1:";
+    for (std::vector<int>::iterator it = a1.begin(); it < a1.end(); it++)
+        std::cout << " " << *it;
+    std::cout << "\n" << std::endl;
+
+    a1.insert(a1.begin() + 5, 10, 100);
+
+    std::cout << "size: " << a1.size() << ", " << a1.capacity() << std::endl;
+    std::cout << "a1:";
+    for (std::vector<int>::iterator it = a1.begin(); it < a1.end(); it++)
+        std::cout << " " << *it;
+    std::cout << "\n" << std::endl;
+
+    std::cout << *a1.end() << "\n" << std::endl;
+
+    ft::vector<int> a2;
+
+    for (int i = 1; i <= 10; i++)
+        a2.push_back(i);
+
+    std::cout << "size: " << a2.size() << ", " << a2.capacity() << std::endl;
+    std::cout << "a2:";
+    for (ft::vector<int>::iterator it = a2.begin(); it < a2.end(); it++)
+        std::cout << " " << *it;
+    std::cout << "\n" << std::endl;
+
+    a2.insert(a2.begin() + 5, 10, 100);
+
+    std::cout << "size: " << a2.size() << ", " << a2.capacity() << std::endl;
+    std::cout << "a2:";
+    for (ft::vector<int>::iterator it = a2.begin(); it < a2.end(); it++)
+        std::cout << " " << *it;
+    std::cout << "\n" << std::endl;
+
+    std::cout << *a2.end() << "\n" << std::endl;
 }
 
 int main()
 {
     try {
         test();
-
-        std::vector<int> v3;
-        std::vector<int> v4;
-
-        v3.push_back(1);
-        v3.push_back(2);
-        v3.push_back(3);
-        v3.push_back(4);
-        v3.push_back(5);
-
-        v4 = v3;
-
-        std::cout << v4.size() << ", " << v4.capacity() << std::endl;
-        std::cout << "v4:";
-        for (std::vector<int>::iterator it = v4.begin(); it < v4.end(); it++)
-            std::cout << " " << *it;
-        std::cout << "\n" << std::endl;
-
-        std::vector<std::string> v5(10);
-        std::vector<std::string> v6;
-
-        std::cout << v5[0] << v5.empty() << std::endl;
-        std::cout << v6.empty() << std::endl;
-
-        std::cout << std::endl;
-
-        std::vector<int> v7(5, 100);
-        ft::vector<int> v8(5, 100);
-
-        std::cout << v7.size() << ", " << v7.capacity() << std::endl;
-        std::cout << "v7:";
-        for (std::vector<int>::iterator it = v7.begin(); it < v7.end(); it++)
-            std::cout << " " << *it;
-        std::cout << "\n" << std::endl;
-
-        std::cout << v8.size() << ", " << v8.capacity() << std::endl;
-        std::cout << "v8:";
-        for (ft::vector<int>::iterator it = v8.begin(); it < v8.end(); it++)
-            std::cout << " " << *it;
-        std::cout << "\n" << std::endl;
-
-        v7.resize(10);
-        v8.resize(10);
-
-        std::cout << v7.size() << ", " << v7.capacity() << std::endl;
-        std::cout << "v7:";
-        for (std::vector<int>::iterator it = v7.begin(); it < v7.end(); it++)
-            std::cout << " " << *it;
-        std::cout << "\n" << std::endl;
-
-        std::cout << v8.size() << ", " << v8.capacity() << std::endl;
-        std::cout << "v8:";
-        for (ft::vector<int>::iterator it = v8.begin(); it < v8.end(); it++)
-            std::cout << " " << *it;
-        std::cout << "\n" << std::endl;
-
     } catch (std::exception &e) {
         std::cerr << e.what() << std::endl;
     }
